40f0e523172df282738666c9be663cc3c5af6b17
VishwaBp <105411775+VishwaBp@users.noreply.github.com>
2025-06-02 12:24:26 +0530

    Expense Tracking Services

---
+++ Expense_Tracking Service/src/test/java/com/vishwanath/expense_tracking_service/ExpenseTrackingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishwanath.expense_tracking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
